da60f1ff69835a0e3b0a4069e0d6bb276f7f5db8
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-08-05 16:17:07 +1200

    Completed Stage 2 (main menu)

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render
+
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(View):
+    template_name = "tickets/menu.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
